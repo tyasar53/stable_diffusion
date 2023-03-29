0e2f3cab0273b06773b38e5c2a729570ab087799
Tolga <tyasar53@gmail.com>
2023-03-29 16:53:12 +0200

    almost done

---
+++ app.py
@@ -0,0 +1,333 @@
+
+import gradio as gr
+import os
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline, DDIMScheduler, StableDiffusionImg2ImgPipeline
+from PIL import Image
+import shutil
+from zipfile import ZipFile
+from helper_functions import construct_concept_objects, construct_json, upload_files, empty_data_dir, adapt_training_config, get_available_models, log_training, get_concepts
+from train_dreambooth import main
+
+MAX_CONCEPTS_TO_SHOW=10
+
+
+#TO DO 
+# uploading multiple images, training on the lattest model
+
+pipe_text2img = None
+pipe_img2img = None
+g_cuda = torch.Generator(device='cuda')
+
+def inference(prompt, negative_prompt, num_samples, height=512, width=512, num_inference_steps=50, guidance_scale=7.5):
+    if (height % 8) != 0 or (width % 8): #check if output image dimensions are mutliples of 8
+        raise gr.Error("Both height and width must be multiples of 8")
+        return
+
+    with torch.autocast("cuda"), torch.inference_mode():
+        return pipe_text2img(
+                prompt, height=int(height), width=int(width),
+                negative_prompt=negative_prompt,
+                num_images_per_prompt=int(num_samples),
+                num_inference_steps=int(num_inference_steps), guidance_scale=guidance_scale,
+                generator=g_cuda
+            ).images
+
+def img2img(prompt, negative_prompt, image, num_samples, guidance_scale=7.5, num_inference_steps=50, strength=0.8):
+  with torch.autocast("cuda"), torch.inference_mode():
+        return pipe_img2img(
+                prompt, image,
+                negative_prompt=negative_prompt,
+                num_images_per_prompt=int(num_samples),
+                num_inference_steps=int(num_inference_steps),
+                guidance_scale=guidance_scale,
+                strength=strength
+            ).images
+
+def init_model(weights_path: str):
+    if weights_path == "":
+        raise gr.Error("No model selected. Please choose from dropdown menu")
+        return
+    if isinstance(weights_path, list):
+        weights_path = weights_path[0]
+    weights_path = weights_path   # If you want to use previously trained model saved in gdrive, replace this with the full path of model in gdrive
+    global pipe_img2img
+    global pipe_text2img
+
+    pipe_text2img         = StableDiffusionPipeline.from_pretrained(weights_path, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+    pipe_img2img = StableDiffusionImg2ImgPipeline.from_pretrained(weights_path, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+    
+    pipe_text2img.scheduler = DDIMScheduler.from_config(pipe_text2img.scheduler.config)
+    pipe_img2img.scheduler = DDIMScheduler.from_config(pipe_img2img.scheduler.config)
+
+    #pipe.enable_xformers_memory_efficient_attention()
+    #pipe_img2img.enable_xformers_memory_efficient_attention()
+
+
+    concepts = get_concepts(weights_path)
+ 
+    return { #you can return object. Keys named same as in outputs parameter of calling event of this function
+        model_load_progress: gr.update(value=f"Successfuly loaded the model: {weights_path}", visible=True),
+        text2img_inference: gr.update(visible=True),
+        img2img_inference:  gr.update(visible=True),
+        prompt_generator: gr.update(visible=True)
+        }
+
+def train(concept_images_1, concept_images_2, concept_images_3,
+            concept_name_1, concept_name_2, concept_name_3,
+            concept_class_name_1, concept_class_name_2, concept_class_name_3,
+            model_name, max_train_steps, weights, use_base_model_checkbox):
+    files           = [concept_images_1,        concept_images_2,       concept_images_3]
+    concept_names   = [concept_name_1,          concept_name_2,         concept_name_3]
+    class_names     = [concept_class_name_1,    concept_class_name_2,   concept_class_name_3]
+
+    concepts = construct_concept_objects(files, concept_names, class_names)
+    if construct_json(concepts) != None:
+        raise gr.Error("Please Provide Concept data")
+        return
+
+    upload_files(concepts)
+    adapt_training_config(model_name, max_train_steps, weights, use_base_model_checkbox)
+    main() #start training of the model
+    log_training(model_name, weights, use_base_model_checkbox) #to be able to load the input images of each concepts later
+
+    return gr.Textbox.update(value=f"Successfully Trained model {model_name}")
+ 
+def variable_outputs(model_path):
+        if model_path == "":
+            raise gr.Error("No model selected. Please choose from dropdown menu")
+            return
+        concepts = get_concepts(model_path)
+        k = len(concepts)
+        update_textboxes = []
+        update_galleries = []
+        for i in range(k):
+            update_textboxes.append(gr.Textbox.update(visible=True, value=concepts[i]["instance_prompt"]))
+            update_galleries.append(gr.Gallery.update(visible=True, value=concepts[i]["instance_images"]))
+
+        return [gr.Markdown.update(visible=True)]+update_textboxes + [gr.Textbox.update(visible=False)]*(MAX_CONCEPTS_TO_SHOW - k) + update_galleries + [gr.Gallery.update(visible=False)] * (MAX_CONCEPTS_TO_SHOW - k) + [gr.Textbox.update(value="Showing Concepts for model below")]
+        #return {
+         #   markdown_concept: gr.Markdown.update(visible=True)
+          #  textboxes_concept: update_textboxes + [gr.Textbox.update(visible=False)]*(MAX_CONCEPTS_TO_SHOW - k)
+          #  galleries_concept: update_galleries + [gr.Gallery.update(visible=False)] * (MAX_CONCEPTS_TO_SHOW - k)
+          #  }
+
+def disable_concepts_view():
+        update_textboxes = []
+        update_galleries = []
+        for i in range(MAX_CONCEPTS_TO_SHOW):
+            update_textboxes.append(gr.Textbox.update(visible=False))
+            update_galleries.append(gr.Gallery.update(visible=False))
+
+        return [gr.Markdown.update(visible=False)] + update_textboxes  + update_galleries + [gr.Textbox.update(value="", visible=False)]
+  
+def update_dropdown_options(dropdown):
+    choices = get_available_models()
+    return gr.Dropdown.update(choices=choices)
+    
+def update_concept_dropdown(weights_path):
+    concepts = get_concepts(weights_path)
+    options_dropdown = []
+    k = len(concepts)
+    for i in range(len(concepts)):
+        options_dropdown.append(concepts[i]["instance_prompt"].replace('photo of', ''))
+    return [gr.Dropdown.update(choices=options_dropdown, visible=True, value="")]* k + [gr.Dropdown.update(visible=False, value="")]*(MAX_CONCEPTS_TO_SHOW - k) + [gr.CheckboxGroup.update(visible=False, value=[])] +[gr.CheckboxGroup.update(visible=True, interactive=True, value=[])] * (k-1) + [gr.CheckboxGroup.update(visible=False, value=[])]*(MAX_CONCEPTS_TO_SHOW - k)
+
+def generate_prompt(
+    d1, d2, d3, d4, d5, d6, d7, d8 , d9, d10,
+    c1, c2, c3, c4, c5, c6, c7, c8 , c9, c10
+
+):
+    #there are as many dropdowns as there are checkboxes. The first half are all dropdowns, the second half are all checkboxes
+    dropdowns =  [d1, d2, d3, d4, d5, d6, d7, d8 , d9, d10]
+    checkboxes = [c1, c2, c3, c4, c5, c6, c7, c8 , c9, c10]
+
+    prompt = "photo of"
+    for i in range(len(dropdowns)):
+        if i == 0: #the first concept  
+           prompt += str(dropdowns[0])
+           continue
+        if dropdowns[i] != "" and len(checkboxes[i]) > 0: #so if there are more then one option selected
+            prompt += " with "
+            if len(checkboxes[i]) == 1:
+                prompt += f"{checkboxes[i][0]} of {dropdowns[i]} and" 
+            else:
+                for j in range(len(checkboxes[i])):
+                    prompt += f"{str(checkboxes[i][j])}, " if len(checkboxes[i]) != (j+1) else f" and {checkboxes[i][j]}"
+
+                prompt += f" of {dropdowns[i]} and"
+               
+
+    return prompt[:-3] #except the last 'and'
+
+    
+    
+dropdown_options = get_available_models()
+
+with gr.Blocks() as demo:
+    gr.Markdown('# Stable Diffusion / Dreambooth - Prototype')
+
+    #Model Selection and Controll
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown('Select the model that you want to use to make inference from')
+            model_dropdown = gr.Dropdown(choices=dropdown_options, interactive=True, multiselect=False)
+        with gr.Column():
+            #Refresh List
+            gr.Markdown('Press this button after training your own concept. It refreshes the list of availabel models')
+            btn_dropdown_refresh = gr.Button(value="Refresh List")
+
+            #Show Concepts Selected
+            gr.Markdown("Press this button to show the trained concepts on the model that you have selected from the dropdown")
+            btn_show_concepts    = gr.Button(value="Show Concepts Selected Model")
+            txt_loading_show_concepts  = gr.Textbox(label="loading status", value="", interactive=False, visible=False)
+            btn_show_concepts.click(fn=lambda: gr.update(visible=True), outputs=[txt_loading_show_concepts])
+       
+            #Load Selected Model
+            gr.Markdown("Press this button AFTER you have selecte a model from the dropdowon")
+            model_load_button    = gr.Button(value="Load Selected Model")
+            model_load_progress  = gr.Textbox(label="loading status", value="", interactive=False, visible=False)
+            model_load_button.click(fn=lambda: gr.update(visible=True), outputs=[model_load_progress])
+
+
+
+
+
+
+        btn_dropdown_refresh.click(update_dropdown_options, outputs=[model_dropdown])
+    
+    #Concepts of a Model 
+    with gr.Row():
+        max_textboxes = 10
+        markdown_concept = gr.Markdown('ATTENTION: This selection will be shown if you select a model. If you want to make inference with this model you always have to clicke the buttion "Load Selected Model"', visible=False)
+        textboxes_concept = []
+        galleries_concept = []
+        for i in range(max_textboxes):
+            t = gr.Textbox(f"Textbox {i}", visible=False, interactive=False)
+            g = gr.Gallery(visible=False)
+            textboxes_concept.append(t)
+            galleries_concept.append(g)
+
+        model_dropdown.change(disable_concepts_view, outputs=[markdown_concept]+ textboxes_concept+galleries_concept + [txt_loading_show_concepts])
+        btn_show_concepts.click(variable_outputs, inputs=[model_dropdown], outputs=[markdown_concept]+ textboxes_concept+galleries_concept + [txt_loading_show_concepts])
+
+        
+    #Inference Tab
+    with gr.Tab("Inference"):
+        with gr.Row(visible=False) as prompt_generator:
+            with gr.Column():
+                gr.Markdown("## Prompt Generator")
+                gr.Markdown("with the provide elemenst below you can generate prompts that combine different aspects of two concepts")
+                with gr.Row():
+                    with gr.Column():
+                        with gr.Row():
+                            dropdowns_prompt_generator = []
+                            checkboxes_prompt_generator= []
+                            for i in range(10): #has to be 10, because of number of attributes generate propmt function takes
+                                with gr.Column():
+                                    dropdowns_prompt_generator.append(gr.Dropdown(label=" concept", interactive=True, multiselect=False,visible=False))
+                                    checkboxes_prompt_generator.append(gr.CheckboxGroup(["shape", "pattern", "color", "length"], label="Elements of Concept 2", info="Select the elements of the concept above that should be included e.g. clicking on pattern shape will add 'with pattern of XXX XXX'", visible=False))
+                        with gr.Row():
+                            btn_combine = gr.Button(value="Generate Prompt")
+                            #txt_generated_prompt = gr.Textbox(label="Generated Prompt")
+
+            
+
+        with gr.Row(visible=False) as text2img_inference:
+            with gr.Column():
+                prompt = gr.Textbox(label="Prompt", value="photo of lur dress", info=" The prompt or prompts to guide the image generation")
+                negative_prompt = gr.Textbox(label="Negative Prompt", value="", info="The prompt or prompts not to guide the image generation")
+                run = gr.Button(value="Generate")
+                with gr.Row():
+                    num_samples = gr.Number(label="Number of Samples", value=4, info="Provide the number of samples that the model generates")
+                    guidance_scale = gr.Number(label="Guidance Scale", value=7.5, info="Higher guidance scale encourages to generate images that are closely linked to the text prompt, usually at the expense of lower image quality")
+                with gr.Row():
+                    height = gr.Number(label="Height", value=512, info="The height in pixels of the generated image. Has to be multiple of 8")
+                    width = gr.Number(label="Width", value=512, info="The width in pixels of the generated image. Has to be mutliple of 8")
+                num_inference_steps = gr.Slider(label="Steps", value=50, maximum=1000, info="The number of denoising steps. More denoising steps usually lead to a higher quality image at the expense of slower inference")
+            with gr.Column():
+                gallery = gr.Gallery()
+        with gr.Row(visible=False) as img2img_inference:
+            with gr.Column():
+                gr.Markdown("## Regenerate from Output")
+                prompt_img2img          = gr.Textbox(label="Img2Img Prompt", value="change the color to blue", interactive=True, info="The prompt or prompts to guide the image generation")
+                negative_prompt_img2img = gr.Textbox(label="Img2Img Negative Prompt", value="", interactive=True, info="The prompt or prompts not to guide the image generation" )
+                regenerate = gr.Button("Regenerate")
+                with gr.Row():
+                    num_samples_img2img    = gr.Number(label="Number of Samples", value=4, info="Provide the number of samples that the model generates")
+                    guidance_scale_img2img = gr.Number(label="Guidance Scale", value=7.5, info="Higher guidance scale encourages to generate images that are closely linked to the text prompt, usually at the expense of lower image quality")  
+                with gr.Row():
+                    num_inference_steps_img2img = gr.Slider(label="Steps", value=50, maximum=1000, info="The number of denoising steps. More denoising steps usually lead to a higher quality image at the expense of slower inference")
+                    strength_img2img = gr.Slider(label="Strength", value=0.8, minimum=0 ,maximum=1, info=" Conceptually, indicates how much to transform the reference image. Must be between 0 and 1. image will be used as a starting point, adding more noise to it the larger the strength. The number of denoising steps depends on the amount of noise initially added. When strength is 1, added noise will be maximum and the denoising process will run for the full number of iterations specified in **Steps**. A value of 1, therefore, essentially ignores image.")
+
+            with gr.Column():
+                gr.Markdown('Drag & Drop the output from the inference here. This serves as input for the regeneration based on the prompt provided on the left')
+                input_img2img = gr.Image(type="pil")
+                gallery_img2img = gr.Gallery()
+    
+    #Training Concepts Tab
+    with gr.Tab('Train New Concept'):
+        with gr.Column() as training_concepts:
+            with gr.Row():
+                gr.Markdown(
+                '''
+                **File:**
+                Plese upload the image files for the concept you want to teach the model. DO ONLY UPLOAD IMAGE FILE (png, jpeg)
+
+
+                **Name of your concept:** 
+                is the name that you will give your concept it should be a word that the model probably has a low prior knowledge. At the same time it should not be a bunch of random characters. If the name is to cryptic, the model will try to print the characters into the image
+                
+                
+                **Class:** 
+                The class name should be a single worded noun that best describes the category you concept belongs to.
+                ''')
+
+            use_base_model_checkbox = gr.Checkbox(label='Use new model for training (if not selected the model selecte from the dropdown above will be selected. If that is left empty then a new model will be used for training)', interactive=True)
+            with gr.Row():
+                concept_images_1 = gr.File(file_count='multiple', interactive=True) 
+                concept_name_1 = gr.Textbox(label="Name of your concept", value="coco", interactive=True)
+                concept_class_name_1   = gr.Textbox(label="Class", value="jacket", interactive=True)
+            with gr.Row():
+                concept_images_2 = gr.File(file_count='multiple', interactive=True) 
+                concept_name_2 = gr.Textbox(label="Name of your concept", value="coco", interactive=True)
+                concept_class_name_2   = gr.Textbox(label="Class", value="jacket", interactive=True)
+            with gr.Row():
+                concept_images_3 = gr.File(file_count='multiple', interactive=True) 
+                concept_name_3 = gr.Textbox(label="Name of your concept", value="coco", interactive=True)
+                concept_class_name_3   = gr.Textbox(label="Class", value="jacket", interactive=True)
+            
+            model_name   = gr.Textbox(label="Model Name", value="my-model", interactive=True)
+            gr.Markdown("Number of optimization steps. A value between 200 and 400 is sufficient in most cases")
+            max_train_steps = gr.Number(label="Number of training steps", value=400, interactive=True)
+
+            train_button = gr.Button(value="Train New Concepts", )
+            concept_textbox = gr.Textbox(visible=True)
+            remove_data_button = gr.Button(value="Remove Data")
+            result_remove_operation = gr.Textbox(label="Result of remove operation", interactive=False)
+            train_button.click(fn=lambda x: gr.Textbox.update(visible=True))
+
+    #Event Listeners
+    train_button.click(train, 
+        inputs=[
+            concept_images_1, concept_images_2, concept_images_3,
+            concept_name_1, concept_name_2, concept_name_3,
+            concept_class_name_1, concept_class_name_2, concept_class_name_3,
+            model_name, max_train_steps, model_dropdown, use_base_model_checkbox
+        ], 
+        outputs=[concept_textbox])
+    run.click(inference, inputs=[prompt, negative_prompt, num_samples, height, width, num_inference_steps, guidance_scale], outputs=gallery)
+    regenerate.click(img2img, inputs=[prompt_img2img, negative_prompt_img2img, input_img2img, num_samples_img2img, guidance_scale_img2img, num_inference_steps_img2img, strength_img2img], outputs=gallery_img2img)
+    remove_data_button.click(empty_data_dir, outputs=[result_remove_operation])
+    model_load_button.click(init_model, inputs=[model_dropdown], outputs=[model_load_progress, text2img_inference, img2img_inference, prompt_generator])
+    btn_dropdown_refresh.click(update_dropdown_options, inputs=[model_dropdown])
+
+
+
+
+    #event listeners for  Prompt Generator 
+    model_dropdown.change(update_concept_dropdown, inputs=[model_dropdown], outputs=dropdowns_prompt_generator + checkboxes_prompt_generator)
+    btn_combine.click(generate_prompt, inputs=dropdowns_prompt_generator+checkboxes_prompt_generator, outputs=[prompt] )
+
+demo.launch(debug=True, share=False)
---
+++ helper_functions.py
@@ -0,0 +1,135 @@
+import os
+import json
+import shutil
+import yaml
+import json
+from PIL import Image
+import gradio as gr
+
+def construct_concept_objects(files, concept_names, class_names):
+    concepts = []
+    for i in range(len(files)):
+        if concept_names[i] != "" and class_names[i] != "" and files[i] != None:
+            concepts.append({
+                "idx": i,
+                "concept_name": concept_names[i],
+                "class_name":   class_names[i],
+                "files": files[i]
+                })
+    return concepts
+
+def construct_json(concepts):
+    if len(concepts) < 1:
+        return gr.Error('Please provide at least one concept to train (Images, Concept name, Class name)')
+    
+
+    concepts_list = []
+    for c in concepts:
+
+        concept_name = c["concept_name"]
+        class_name = c["class_name"]
+
+        concept = {
+         "instance_prompt":      f"photo of {concept_name} {class_name}",
+         "class_prompt":         f"photo of a {class_name}",
+         "instance_data_dir":    f"{ os.path.join(os.getcwd(),f'data/{concept_name}')}",
+         "class_data_dir":       f"{ os.path.join(os.getcwd(),f'data/{class_name}')}"
+        }
+        os.makedirs(concept["instance_data_dir"], exist_ok=True)
+        concepts_list.append(concept)
+
+        with open("concepts_list.json", "w") as f:
+            json.dump(concepts_list, f, indent=4)
+   
+def upload_files(concepts):
+    for concept in concepts:
+        for idx, file in enumerate(concept["files"]):
+            d = open(file.name, 'rb')
+            d.seek(0)
+
+            f = open(os.path.join(os.getcwd(),'data', concept["concept_name"], file.name.split('/')[-1]), "wb")
+            f.write(d.read())
+            
+            d.close()
+            f.close()
+
+def empty_data_dir():
+    data_dirs = next(os.walk('./data'))[1]
+    for folder in data_dirs:
+        shutil.rmtree(os.path.join(os.getcwd(), 'data', folder))
+    return "All image folders have been removed"
+
+def adapt_training_config(model_name, max_train_steps, weights, use_base_model_checkbox):
+    with open(os.path.join(os.getcwd(), 'train.config.yml'), 'r') as stream:
+        args=yaml.safe_load(stream)
+    
+    args['output_dir'] = os.path.join(os.getcwd(), 'weights', model_name)
+    args['max_train_steps'] = int(max_train_steps)
+    if weights != None and use_base_model_checkbox != True:
+        args['pretrained_model_name_or_path'] = weights
+    else:
+        args['pretrained_model_name_or_path'] = 'runwayml/stable-diffusion-v1-5'
+
+        
+
+    with open(os.path.join(os.getcwd(), 'train.config.yml'), 'w') as config:
+        config.write(yaml.dump(args))
+
+
+def get_available_models():
+    models = []
+    for i in next(os.walk('./weights'))[1]:
+        for j in next(os.walk(f'./weights/{i}'))[1]:
+            models.append(os.path.join(os.getcwd(), 'weights', i, j))
+    return models
+
+def log_training(model_path, selecte_as_prior_weights, use_base_model_checkbox):
+
+    config = {}
+    with open(os.path.join(os.getcwd(), 'train.config.yml'), 'r') as stream:
+        config=yaml.safe_load(stream)
+        print(config)
+
+    with open(os.path.join(os.getcwd(), 'concepts_list.json'), 'r') as json_file:
+        concepts_list = json.load(json_file)
+    
+    with open(os.path.join(os.getcwd(), 'train.log.yml'), 'r') as stream:
+        logs=yaml.safe_load(stream)
+
+
+    if selecte_as_prior_weights != None and use_base_model_checkbox != True:
+        for log in logs: 
+            if log['weights_path'] == selecte_as_prior_weights:
+                concepts_list = concepts_list + log['concepts_list']
+
+    logs.append({
+    "weights_path": os.path.join(os.getcwd(), 'weights', model_path, str(config['max_train_steps'])),
+    "concepts_list": concepts_list
+    })
+
+    with open(os.path.join(os.getcwd(), 'train.log.yml'), 'w') as config:
+        config.write(yaml.dump(logs))
+
+def get_concepts(weights_path):
+    concepts = []
+    logs = {}
+    with open(os.path.join(os.getcwd(), 'train.log.yml'), 'r') as stream:
+        logs=yaml.safe_load(stream)
+
+    for train_run in logs:
+        print(f"train run paths: {train_run['weights_path']}  == {weights_path}")
+        if train_run['weights_path'] == weights_path:
+            concepts = train_run['concepts_list']
+            break
+    
+    concepts_to_return = []
+    for concept in concepts:
+        images = []
+        for image_path in next(os.walk(concept['instance_data_dir']))[2]:
+            images.append(
+                Image.open(os.path.join(concept['instance_data_dir'], image_path))
+            )
+        
+        concepts_to_return.append( {"instance_prompt": concept['instance_prompt'], "class_prompt": concept['class_prompt'], "instance_images": images} )
+
+    return concepts_to_return
